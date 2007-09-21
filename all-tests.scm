c840fa134aa6337c8fdbb3cc04e119795c287183
Alexey Radul <axch@mit.edu>
2007-09-21 23:09:52 +0000

    Refactored the old test-running to actually use set-up (to implement a fixture), and added two tests that verify that run-test triggers all the right events (and none of the wrong ones).

--- all-tests.scm
+++ all-tests.scm
@@ -40,93 +40,143 @@
      (eq? (tg:get mock-test-group '(subgroup-1 repeated-name))
 	  (tg:get mock-test-group '(subgroup-2 repeated-name))))))
 
-(define-test (test-running)
-  (let ((mock-test-group (make-test-group 'mockery))
-	(events '()))
-    (define (add-event event)
-      (set! events (cons event events)))
-    (with-top-level-group 
-     mock-test-group
-     (lambda ()
-       (define-group-surround
-	 (add-event 'top-group-surround-begin)
-	 (run-test)
-	 (add-event 'top-group-surround-end))
-       (define-group-set-up (add-event 'top-group-set-up))
-       (define-group-tear-down (add-event 'top-group-tear-down))
-       (define-surround
-	 (add-event 'top-surround-begin)
-	 (run-test)
-	 (add-event 'top-surround-end))
-       (define-set-up (add-event 'top-set-up))
-       (define-tear-down (add-event 'top-tear-down))
-       (in-test-group
-	group-a
+(in-test-group
+ event-tracking-tests
+ (let ((mock-test-group (make-test-group 'mockery))
+       (events '()))
+   (define (add-event event)
+     (set! events (cons event events)))
+   (define-set-up
+     (set! events '())
+     (set! mock-test-group (make-test-group 'mockery))
+     (with-top-level-group 
+      mock-test-group
+      (lambda ()
 	(define-group-surround
-	  (add-event 'a-group-surround-begin)
+	  (add-event 'top-group-surround-begin)
 	  (run-test)
-	  (add-event 'a-group-surround-end))
-	(define-group-set-up (add-event 'a-group-set-up))
-	(define-group-tear-down (add-event 'a-group-tear-down))
+	  (add-event 'top-group-surround-end))
+	(define-group-set-up (add-event 'top-group-set-up))
+	(define-group-tear-down (add-event 'top-group-tear-down))
 	(define-surround
-	  (add-event 'a-surround-begin)
+	  (add-event 'top-surround-begin)
 	  (run-test)
-	  (add-event 'a-surround-end))
-	(define-set-up (add-event 'a-set-up))
-	(define-tear-down (add-event 'a-tear-down))
-	(define-test (test-a1)
-	  (add-event 'test-a1)
-	  (assert-= 5 (+ 2 2)))
-	(define-test (test-a2)
-	  (add-event 'test-a2)
-	  (assert-equal '() #f)))
-       (in-test-group
-	group-b
-	(define-group-surround
-	  (add-event 'b-group-surround-begin)
-	  (run-test)
-	  (add-event 'b-group-surround-end))
-	(define-group-set-up (add-event 'b-group-set-up))
-	(define-group-tear-down (add-event 'b-group-tear-down))
-	(define-surround
-	  (add-event 'b-surround-begin)
-	  (run-test)
-	  (add-event 'b-surround-end))
-	(define-set-up (add-event 'b-set-up))
-	(define-tear-down (add-event 'b-tear-down))
-	(define-test (test-b1)
-	  (add-event 'test-b1)
-	  (foo))
-	(define-test (test-b2)
-	  (add-event 'test-b2)
-	  (assert-= 4 (+ 2 (/ 2 0)))))))
-    (let ((result-string
-	   (with-output-to-string
-	     (lambda ()
-	       (with-top-level-group
-		mock-test-group
-		(lambda ()
-		  (run-registered-tests)))))))
-      (assert-matches
-       "4 tests, 2 failures, 2 errors"
-       result-string))
-    (assert-equal
-     ;; Beware, the indentation in this list was produced manually
-     '(top-group-surround-begin top-group-set-up
-       top-surround-begin top-set-up
-        a-group-surround-begin a-group-set-up
-         a-surround-begin a-set-up test-a1 a-tear-down a-surround-end
-	 a-surround-begin a-set-up test-a2 a-tear-down a-surround-end
-        a-group-tear-down a-group-surround-end
-       top-tear-down top-surround-end
-       top-surround-begin top-set-up
-        b-group-surround-begin b-group-set-up
-	 b-surround-begin b-set-up test-b1 b-tear-down b-surround-end
-	 b-surround-begin b-set-up test-b2 b-tear-down b-surround-end
-	b-group-tear-down b-group-surround-end
-       top-tear-down top-surround-end
-       top-group-tear-down top-group-surround-end)
-     (reverse events))))
+	  (add-event 'top-surround-end))
+	(define-set-up (add-event 'top-set-up))
+	(define-tear-down (add-event 'top-tear-down))
+	(in-test-group
+	 group-a
+	 (define-group-surround
+	   (add-event 'a-group-surround-begin)
+	   (run-test)
+	   (add-event 'a-group-surround-end))
+	 (define-group-set-up (add-event 'a-group-set-up))
+	 (define-group-tear-down (add-event 'a-group-tear-down))
+	 (define-surround
+	   (add-event 'a-surround-begin)
+	   (run-test)
+	   (add-event 'a-surround-end))
+	 (define-set-up (add-event 'a-set-up))
+	 (define-tear-down (add-event 'a-tear-down))
+	 (define-test (test-a1)
+	   (add-event 'test-a1)
+	   (assert-= 5 (+ 2 2)))
+	 (define-test (test-a2)
+	   (add-event 'test-a2)
+	   (assert-equal '() #f)))
+	(in-test-group
+	 group-b
+	 (define-group-surround
+	   (add-event 'b-group-surround-begin)
+	   (run-test)
+	   (add-event 'b-group-surround-end))
+	 (define-group-set-up (add-event 'b-group-set-up))
+	 (define-group-tear-down (add-event 'b-group-tear-down))
+	 (define-surround
+	   (add-event 'b-surround-begin)
+	   (run-test)
+	   (add-event 'b-surround-end))
+	 (define-set-up (add-event 'b-set-up))
+	 (define-tear-down (add-event 'b-tear-down))
+	 (define-test (test-b1)
+	   (add-event 'test-b1)
+	   (foo))
+	 (define-test (test-b2)
+	   (add-event 'test-b2)
+	   (assert-= 4 (+ 2 (/ 2 0))))))))
+   (define-test (test-running)
+     (let ((result-string
+	    (with-output-to-string
+	      (lambda ()
+		(with-top-level-group
+		 mock-test-group
+		 (lambda ()
+		   (run-registered-tests)))))))
+       (assert-matches
+	"4 tests, 2 failures, 2 errors"
+	result-string))
+     (assert-equal
+      ;; Beware, the indentation in this list was produced manually
+      '(top-group-surround-begin top-group-set-up
+	top-surround-begin top-set-up
+	 a-group-surround-begin a-group-set-up
+	  a-surround-begin a-set-up test-a1 a-tear-down a-surround-end
+	  a-surround-begin a-set-up test-a2 a-tear-down a-surround-end
+	 a-group-tear-down a-group-surround-end
+	top-tear-down top-surround-end
+	top-surround-begin top-set-up
+	 b-group-surround-begin b-group-set-up
+	  b-surround-begin b-set-up test-b1 b-tear-down b-surround-end
+	  b-surround-begin b-set-up test-b2 b-tear-down b-surround-end
+	 b-group-tear-down b-group-surround-end
+	top-tear-down top-surround-end
+	top-group-tear-down top-group-surround-end)
+      (reverse events)))
+   (define-test (test-running-one-test)
+     (let ((result-string
+	    (with-output-to-string
+	      (lambda ()
+		(with-top-level-group
+		 mock-test-group
+		 (lambda ()
+		   (run-test '(group-b test-b1))))))))
+       (assert-matches
+	"1 tests, 0 failures, 1 errors"
+	result-string))
+     (assert-equal
+      ;; Beware, the indentation in this list was produced manually
+      '(top-group-surround-begin top-group-set-up
+	top-surround-begin top-set-up
+	 b-group-surround-begin b-group-set-up
+	  b-surround-begin b-set-up test-b1 b-tear-down b-surround-end
+	 b-group-tear-down b-group-surround-end
+	top-tear-down top-surround-end
+	top-group-tear-down top-group-surround-end)
+      (reverse events)))
+   (define-test (test-running-one-group)
+     (let ((result-string
+	    (with-output-to-string
+	      (lambda ()
+		(with-top-level-group
+		 mock-test-group
+		 (lambda ()
+		   (run-test '(group-a))))))))
+       (assert-matches
+	"2 tests, 2 failures, 0 errors"
+	result-string))
+     (assert-equal
+      ;; Beware, the indentation in this list was produced manually
+      '(top-group-surround-begin top-group-set-up
+	top-surround-begin top-set-up
+	 a-group-surround-begin a-group-set-up
+	  a-surround-begin a-set-up test-a1 a-tear-down a-surround-end
+	  a-surround-begin a-set-up test-a2 a-tear-down a-surround-end
+	 a-group-tear-down a-group-surround-end
+	top-tear-down top-surround-end
+	top-group-tear-down top-group-surround-end)
+      (reverse events)))))
+
+;; TODO test error behavior, particularly of run-test
 
 (let ((entered-group #f))
   (in-test-group
